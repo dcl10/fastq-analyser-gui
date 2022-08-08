77a959c9eabfe86efebb3746813655d2b313f81a
Daniel Lea <dan.carr.lea@gmail.com>
2022-08-08 16:25:07 +0100

    draft text input

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,29 @@
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input
+} from '@chakra-ui/react'
+
+function TextInput({isInvalid = false, title}) {
+  return (
+    <FormControl isInvalid={isInvalid}>
+      <FormLabel>{title}</FormLabel>
+      <Input />
+      {
+        formInvalid ? (
+          <FormErrorMessage>
+            Please paste at least one valid fastq record.
+          </FormErrorMessage>
+        ) : (
+          <FormHelperText>
+            Paste one or more fastq records.
+          </FormHelperText>
+        )
+      }
+    </FormControl>
+  )
+}
+
+export default TextInput

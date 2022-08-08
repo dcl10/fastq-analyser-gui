5bfd596496381bea55424a2a30b37f3a9da8cca1
Daniel Lea <dan.carr.lea@gmail.com>
2022-08-08 17:42:28 +0100

    layout for input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,60 @@
 import { useState } from 'react'
 import { 
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
   Button,
+  ButtonGroup,
+  Box,
   Container,
   Heading,
-  HStack,
-  Input,
   Text,
+  Spacer,
+  Center,
 } from '@chakra-ui/react'
 import { invoke } from '@tauri-apps/api'
+import FileInput from './components/FileInput'
+import TextInput from './components/TextInput'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  /**
-   * A slightly contrived way to increase the count
-   * to show how to invoke Rust commands from the frontend
-   */
-  const rustIncrease = async () => {
-    let newNumber = await invoke('increase', {num: count})
-    setCount(newNumber)
-  }
-
-  /**
-   * A slightly contrived way to decrease the count
-   * to show how to invoke Rust commands from the frontend
-   */
-  const rustDecrease = async () => {
-    let newNumber = await invoke('decrease', {num: count})
-    setCount(newNumber)
-  }
 
   return (
     <Container className='App'>
-      <Heading>This is a template Tauri app.</Heading>
-      <Text>It combines a fast and safe Rust backend with a React frontend.</Text>
-      <HStack>
-        <Button onClick={rustDecrease}>-</Button>
-        <Input value={count} isReadOnly />
-        <Button onClick={rustIncrease}>+</Button>
-      </HStack>
+      <Heading>Fastq Analyser</Heading>
+      {/* The input options */}
+      <Accordion>
+        <AccordionItem>
+          <AccordionButton>
+            <Text>Input Text</Text>
+            <Spacer />
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel>
+            <TextInput title={'Paste fastq'} />
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionButton>
+            <Text>Input File</Text>
+            <Spacer />
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel>
+            <FileInput title={'Upload Fastq file'}/>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+
+      {/* Control buttons */}
+      <Center>
+        <ButtonGroup spacing={4}>
+          <Button colorScheme='blue'>Submit</Button>
+          <Button variant='outline' colorScheme='red'>Clear</Button>
+        </ButtonGroup>
+      </Center>
     </Container>
   )
 }

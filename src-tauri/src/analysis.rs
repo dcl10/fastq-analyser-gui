5acef586b6a44c2d42477dc79df3545164b98c85
Daniel Lea <dan.carr.lea@gmail.com>
2022-08-10 12:31:38 +0100

    Sequence analysis (#3)
    * initialise analytics.rs
    
    * `SeqResult` struct
    
    * add `rust-bio` dep
    
    * fill out function for analysing sequence
    
    * check validity of records
    
    * add `id` and `desc` to `SeqResult`
    
    * fix error on missing description

---
+++ src-tauri/src/analysis.rs
@@ -0,0 +1,74 @@
+use bio::io::fastq;
+use bio::seq_analysis::{gc, orf};
+use serde::{Deserialize, Serialize};
+
+#[derive(Default)]
+#[derive(Deserialize, Serialize)]
+pub struct SeqResult {
+  id: String,
+  desc: String,
+  gc: f32,
+  n_orfs: usize,
+  is_valid: bool,
+}
+
+
+#[tauri::command]
+pub fn analyse_sequences(sequences: String) -> Vec<SeqResult> {
+  let mut results = Vec::new();
+  let reader = fastq::Reader::new(sequences.as_bytes());
+  let records = reader.records().map(|rec| rec.unwrap());
+
+  // Hyperparameters for finding open reading frames (ORFs).
+  // NB: DNA alphabet
+  let start_codons = vec![b"ATG"];
+  let stop_codons = vec![b"TGA", b"TAG", b"TAA"];
+  let min_len = 50;
+  let finder = orf::Finder::new(start_codons, stop_codons, min_len);
+
+  // Iterate over results and find GC content and ORFs
+  for rec in records {
+    let gc_ = gc::gc_content(rec.seq());
+    let n_orfs = finder.find_all(rec.seq()).count();
+    results.push(
+      SeqResult {
+        n_orfs,
+        id: rec.id().to_owned(),
+        desc: rec.desc().unwrap_or("").to_owned(),
+        gc: gc_,
+        is_valid: rec.check().is_ok()
+      }
+    );
+  }
+
+  results
+}
+
+#[cfg(test)]
+mod tests{
+    use crate::analysis::analyse_sequences;
+
+  #[test]
+  fn test_analyse_sequences() {
+    let mut fqs_str: String = "@id description\nATAT\n+\n!!!!\n".to_owned();
+    fqs_str.push_str("@id description\nGCGC\n+\n!!!!\n");
+
+    let results = analyse_sequences(fqs_str);
+    assert_eq!(results.len(), 2);
+  }
+
+  #[test]
+  fn test_invalid_records() {
+    let missing_sequence: String = "@id description\n\n+\n!!!!\n".to_owned();
+
+    let results = analyse_sequences(missing_sequence);
+    assert_eq!(results.len(), 1);
+    assert!(!results[0].is_valid);
+
+    let missing_description: String = "@id \nATAT\n+\n!!!!\n".to_owned();
+
+    let results = analyse_sequences(missing_description);
+    assert_eq!(results.len(), 1);
+    assert!(results[0].is_valid);
+  }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,41 +3,11 @@
   windows_subsystem = "windows"
 )]
 
+mod analysis;
+
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![increase, decrease])
+    .invoke_handler(tauri::generate_handler![analysis::analyse_sequences])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
-
-/// A tauri command to return the supplied number `num` + 1.
-/// 
-/// # Arguments:
-/// * num: the number to be increased.
-/// 
-/// # Example
-/// `example.js`
-/// ```javascript
-/// invoke('increase', {num: 1})
-/// // 2
-/// ```
-#[tauri::command]
-fn increase(num: i32) -> i32 {
-  num + 1
-} 
-
-/// A tauri command to return the supplied number `num` - 1.
-/// 
-/// # Arguments:
-/// * num: the number to be decreased.
-/// 
-/// # Example
-/// `example.js`
-/// ```javascript
-/// invoke('decrease', {num: 1})
-/// // 0
-/// ```
-#[tauri::command]
-fn decrease(num: i32) -> i32 {
-  num - 1
-} 

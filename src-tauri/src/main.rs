f9a07edf7842917ffe1b4ddb8a9a4d1c08cc77a8
Daniel Lea <dan.carr.lea@gmail.com>
2022-08-10 17:09:07 +0100

    format backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,13 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 mod analysis;
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![analysis::analyse_sequences])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![analysis::analyse_sequences])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }

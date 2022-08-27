b9de705f5bd1a493e0ad662ec080fd7450ad87e4
dcl10 <dan.carr.lea@gmail.com>
2022-08-27 14:06:44 +0100

    display length in results modal

--- src/App.jsx
+++ src/App.jsx
@@ -114,6 +114,9 @@ function App() {
                           <strong>Record is valid?</strong>&nbsp;{result.is_valid ? 'Yes' : 'No'}
                         </Text>
                         <Text>
+                          <strong>Sequence length:</strong>&nbsp;{result.seq_len} bases
+                        </Text>
+                        <Text>
                           <strong>PHRED score per base:</strong>&nbsp;{result.phred_score / result.seq_len}
                         </Text>
                         <Text>

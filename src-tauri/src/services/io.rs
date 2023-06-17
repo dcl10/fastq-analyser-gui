03eee17554a743f41c3097ed5aac2f74944f4575
dcl10 <dan.carr.lea@gmail.com>
2023-06-17 18:32:02 +0100

    save results

--- src-tauri/src/services/io.rs
+++ src-tauri/src/services/io.rs
@@ -1,8 +1,9 @@
 use bio::io::{fasta, fastq};
 use flate2::read::GzDecoder;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use std::fs::File;
-use std::io::{BufReader, Read, Write};
+use std::io::ErrorKind::InvalidData;
+use std::io::{BufReader, BufWriter, Read, Write};
 use std::path::Path;
 
 pub fn read_fasta(path: &Path) -> fasta::Reader<BufReader<File>> {
@@ -25,11 +26,27 @@ pub fn read_fastq(path: &Path) -> fastq::Reader<BufReader<File>> {
     }
 }
 
-pub fn save_results<T>(results: Vec<T>, dest: &Path) -> Result<(), std::io::Error>
+pub fn save_results<T>(results: &Vec<T>, dest: &Path) -> Result<(), std::io::Error>
 where
     T: Serialize,
 {
-    todo!()
+    let serialised_results = serde_json::to_string_pretty(results);
+    if serialised_results.is_err() {
+        return Err(std::io::Error::new(InvalidData, "Could not save results.s"));
+    }
+    // binding prevents "drop of value while in use" warning
+    let binding = String::from(serialised_results.unwrap());
+    let results_as_bytes = binding.as_bytes();
+
+    let output_file = File::create(dest);
+
+    match output_file {
+        Ok(file) => {
+            let mut writer = BufWriter::new(file);
+            return writer.write_all(results_as_bytes);
+        }
+        Err(error) => return Err(error),
+    }
 }
 
 fn extract_gzip(path: &Path) -> String {
@@ -58,9 +75,10 @@ mod tests {
         let save_dir = tauri::api::path::desktop_dir().unwrap();
         let save_file = Uuid::new_v4().to_string() + ".json";
         let save_dest = save_dir.join(save_file);
+        println!("{}", save_dest.to_str().unwrap());
 
         // Act
-        let save_action = save_results(results, save_dest.as_path());
+        let save_action = save_results(&results, save_dest.as_path());
 
         // Assert
         assert!(save_action.is_ok());
@@ -76,7 +94,7 @@ mod tests {
         let save_dest = save_dir.join(save_file);
 
         // Act
-        let save_action = save_results(results, save_dest.as_path());
+        let save_action = save_results(&results, save_dest.as_path());
 
         // Assert
         assert!(save_action.is_ok());
@@ -87,12 +105,14 @@ mod tests {
     fn test_save_results_errors_on_nonexistent_dest() {
         // Arrange
         let results = Vec::from_iter([FastqSeqResult::default()]);
-        let save_dir = tauri::api::path::desktop_dir().unwrap();
+        let save_dir = tauri::api::path::desktop_dir()
+            .unwrap()
+            .join(Uuid::new_v4().to_string());
         let save_file = Uuid::new_v4().to_string() + ".json";
-        let save_dest = save_dir.join(Uuid::new_v4().to_string() + &save_file);
+        let save_dest = save_dir.join(save_file);
 
         // Act
-        let save_action = save_results(results, save_dest.as_path());
+        let save_action = save_results(&results, save_dest.as_path());
 
         // Assert
         assert!(save_action.is_err());

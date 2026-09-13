1a22cb4d5594e4e89cf0d7ffaf3f4da60838f349
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2026-09-13 12:41:25 +0100

    scaffold data module

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,28 +1,29 @@
 mod analysis;
+mod data;
 mod models;
 mod services;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_dialog::init())
-    .plugin(tauri_plugin_os::init())
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![
-      analysis::commands::analyse_fastq_sequences,
-      analysis::commands::analyse_fastq_file,
-      analysis::commands::analyse_fasta_sequences,
-      analysis::commands::analyse_fasta_file
-    ])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_os::init())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            analysis::commands::analyse_fastq_sequences,
+            analysis::commands::analyse_fastq_file,
+            analysis::commands::analyse_fasta_sequences,
+            analysis::commands::analyse_fasta_file
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }

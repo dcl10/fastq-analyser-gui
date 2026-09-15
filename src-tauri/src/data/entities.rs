1fe2ec08a9ad58ad44d2a38ee6c4a4d8f2f3257c
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2026-09-15 20:11:32 +0100

    from impls for entities

--- src-tauri/src/data/entities.rs
+++ src-tauri/src/data/entities.rs
@@ -1,6 +1,6 @@
 use sqlx::types::chrono::{self, Utc};
 
-use crate::models::{FastaSeqResult, FastqSeqResult};
+use crate::models::{FastaSeqResult, FastqSeqResult, Run as RunModel, RunRecords};
 
 #[derive(Debug, Clone, sqlx::FromRow)]
 pub struct Run {
@@ -8,6 +8,29 @@ pub struct Run {
     pub created_at: chrono::DateTime<Utc>,
     pub records: Option<Vec<Record>>,
     pub result_type: ResultType,
+}
+
+impl From<RunModel> for Run {
+    fn from(value: RunModel) -> Self {
+        let records: Vec<Record> = match value.records {
+            RunRecords::FastaRecords(fasta_seq_results) => {
+                fasta_seq_results.into_iter().map(Into::into).collect()
+            }
+            RunRecords::FastqRecords(fastq_seq_results) => {
+                fastq_seq_results.into_iter().map(Into::into).collect()
+            }
+        };
+        Run {
+            id: value.id,
+            created_at: value.created_at,
+            records: if !records.is_empty() {
+                Some(records)
+            } else {
+                None
+            },
+            result_type: value.result_type,
+        }
+    }
 }
 
 #[derive(Debug, Clone, sqlx::FromRow)]
@@ -45,6 +68,36 @@ impl Into<FastqSeqResult> for Record {
             is_valid: self.is_valid,
             seq_len: self.seq_len,
             phred_score: self.phred_score.unwrap(),
+        }
+    }
+}
+
+impl From<FastaSeqResult> for Record {
+    fn from(value: FastaSeqResult) -> Self {
+        Record {
+            id: 0,
+            seq_id: value.id,
+            description: value.desc,
+            gc_content: value.gc,
+            n_orfs: value.n_orfs,
+            is_valid: value.is_valid,
+            seq_len: value.seq_len,
+            phred_score: None,
+        }
+    }
+}
+
+impl From<FastqSeqResult> for Record {
+    fn from(value: FastqSeqResult) -> Self {
+        Record {
+            id: 0,
+            seq_id: value.id,
+            description: value.desc,
+            gc_content: value.gc,
+            n_orfs: value.n_orfs,
+            is_valid: value.is_valid,
+            seq_len: value.seq_len,
+            phred_score: Some(value.phred_score),
         }
     }
 }

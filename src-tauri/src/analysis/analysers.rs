b63ea40862ca910943c337995199d3cbbcbec8d7
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2026-09-25 12:39:49 +0100

    add error decription to fastq too

--- src-tauri/src/analysis/analysers.rs
+++ src-tauri/src/analysis/analysers.rs
@@ -22,8 +22,9 @@ pub fn analyse_fastq_records(records: &Vec<fastq::Record>) -> Vec<FastqSeqResult
                 seq_len: (rec.seq().len() as u32),
                 ..Default::default()
             },
-            Err(_) => FastqSeqResult {
+            Err(e) => FastqSeqResult {
                 id: "Invalid Record".to_owned(),
+                desc: Some(e.to_string()),
                 is_valid: false,
                 ..Default::default()
             },
